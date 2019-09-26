efc9847110e3356bd17c9f01241cd506a37a65ff
cris3000 <chriswhealey1@gmail.com>
2019-09-26 23:13:48 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -2,11 +2,10 @@
 session_start();
 
 
-	$host= "chealey01.lampt.eeecs.qub.ac.uk";
+	$host= "xxxxxx.lampt.eeecs.qub.ac.uk";
 	$pw= "1ZRScm6G8nR570xj";
-	$db="chealey01";
-	$user="chealey01";
-
+	$db="xxxxx01";
+	$user="xxxxx01";
 
 $mysqli = new mysqli($host,$user,$pw,$db);
 
