a90ac7787217211f24fdff13ad5891a0bef6268b
cris3000 <chriswhealey1@gmail.com>
2019-09-26 23:22:05 +0100

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -6,11 +6,10 @@ if (!isset($_SESSION['40242807_id']))
    {
       header("location: Login.php");
 	  
-   }
-	$host= "chealey01.lampt.eeecs.qub.ac.uk";
-	$pw= "1ZRScm6G8nR570xj";
-	$db="chealey01";
-	$user="chealey01";
+   }$host= "xxxxxxxxeecs.qub.ac.uk";
+	$pw= "1ZRxxxScm6G8nR570xj";
+	$db="xxxx";
+	$user="x";
 	
 
 $mysqli = new mysqli($host,$user,$pw,$db);
